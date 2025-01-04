8d2220e80b3f65483317fc4153f02c436031e61b
Anton Kucha <anton.kucha@aionys.com>
2025-01-04 17:01:29 +0100

    Fixes

---
+++ src/main/java/com/github/ku4marez/commonlibraries/entity/entity/enums/Role.java
@@ -0,0 +1,5 @@
+package com.github.ku4marez.commonlibraries.entity.entity.enums;
+
+public enum Role {
+    ADMIN, DOCTOR, PATIENT
+}
--- src/main/java/com/github/ku4marez/commonlibraries/entity/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.github.ku4marez.commonlibraries.entity.enums;
-
-public enum Role {
-    ADMIN, DOCTOR, PATIENT
-}
