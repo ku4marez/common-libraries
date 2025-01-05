76922fc471a0dfcd916a701221be9d5ad882e651
Anton Kucha <anton.kucha@aionys.com>
2025-01-05 17:57:29 +0100

    Add cors config util

---
+++ src/main/java/com/github/ku4marez/commonlibraries/entity/util/CorsConfigurationUtil.java
@@ -0,0 +1,20 @@
+package com.github.ku4marez.commonlibraries.entity.util;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfigurationUtil {
+
+    public static WebMvcConfigurer corsConfigurer(String allowedOrigin) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
