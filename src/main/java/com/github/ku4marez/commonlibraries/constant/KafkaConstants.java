d2863f842f229dbbcc512013e8a4f45c5c382eaa
Anton Kucha <anton.kucha@aionys.com>
2025-01-08 20:48:42 +0100

    Update constants

--- src/main/java/com/github/ku4marez/commonlibraries/constant/KafkaConstants.java
+++ src/main/java/com/github/ku4marez/commonlibraries/constant/KafkaConstants.java
@@ -2,5 +2,10 @@ package com.github.ku4marez.commonlibraries.constant;
 
 public class KafkaConstants {
     public static final String APPOINTMENT_CREATED_TOPIC = "appointment.created";
+    public static final String APPOINTMENT_UPDATED_TOPIC = "appointment.updated";
+
+    public static final String APPOINTMENT_CREATED_EVENT = "APPOINTMENT_CREATED";
+    public static final String APPOINTMENT_UPDATED_EVENT = "APPOINTMENT_UPDATED";
+
     public static final String NOTIFICATION_SENT_TOPIC = "notification.sent";
 }
