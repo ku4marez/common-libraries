b28491420561e71ed64f16ca7e2bce89de2e591e
Anton Kucha <anton.kucha@aionys.com>
2025-02-10 23:56:00 +0100

    Fix entity audit issue

--- src/main/java/com/github/ku4marez/commonlibraries/entity/common/PersistentAuditedEntity.java
+++ src/main/java/com/github/ku4marez/commonlibraries/entity/common/PersistentAuditedEntity.java
@@ -1,24 +1,32 @@
 package com.github.ku4marez.commonlibraries.entity.common;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class PersistentAuditedEntity extends PersistentObject {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Document
+public abstract class PersistentAuditedEntity extends PersistentObject {
+
     @CreatedBy
+    @Column(updatable = false)
     private String createdBy;
 
     @LastModifiedBy
     private String updatedBy;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime creationDate;
 
     @LastModifiedDate
