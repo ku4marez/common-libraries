eb24598ea4dfbb9aea5535199f51efa6ba6e53db
Anton Kucha <anton.kucha@aionys.com>
2025-02-10 23:02:46 +0100

    Add patient created kafka event

--- src/main/java/com/github/ku4marez/commonlibraries/constant/KafkaConstants.java
+++ src/main/java/com/github/ku4marez/commonlibraries/constant/KafkaConstants.java
@@ -7,5 +7,8 @@ public class KafkaConstants {
     public static final String APPOINTMENT_CREATED_EVENT = "APPOINTMENT_CREATED";
     public static final String APPOINTMENT_UPDATED_EVENT = "APPOINTMENT_UPDATED";
 
+    public static final String PATIENT_CREATED_TOPIC = "patient.created";
+    public static final String PATIENT_CREATED_EVENT = "PATIENT_CREATED";
+
     public static final String NOTIFICATION_SENT_TOPIC = "notification.sent";
 }
